4d72c690c8a264d5b4534a776055e29203612e55
BossNz <teerawatbossnz@gmail.com>
2024-02-03 00:45:30 +0700

    👻 update: ex05

---
+++ cell03/ex03/todo.js
@@ -0,0 +1,56 @@
+function addItem() {
+  var text = prompt("This is a prompt box", "this is simple text");
+  var list = new Date();
+  setCookie("obj" + list.getTime(), encodeURIComponent(text));
+  checkForObjectives();
+}
+
+function setCookie(sName, sValue) {
+  document.cookie = sName + "=" + escape(sValue);
+  var date = new Date();
+  date.setMonth(date.getYear() + 1);
+  document.cookie += "; expires=" + date.toUTCString();
+}
+
+function unsetCookie(sName) {
+  document.cookie = sName + "=; expires=Fri, 31 Dec 1999 23:59:59 GMT;";
+}
+
+function checkForObjectives() {
+  var list = document.getElementById("list");
+  list.innerHTML = "";
+  var cookies = document.cookie.split("; ");
+  cookies.sort(
+    (a, b) =>
+      b.replace("obj", "").split("=")[0] - a.replace("obj", "").split("=")[0]
+  );
+  for (var i = 0; i < cookies.length; i++) {
+    var part = cookies[i].split("=");
+    if (part[0].indexOf("obj") === 0) {
+      try {
+        addListItem(part[0], decodeURI(decodeURI(part[1])));
+      } catch (error) {
+        console.log(error.message + " " + part[1]);
+      }
+    }
+  }
+}
+
+function addListItem(id, text) {
+  var list = document.getElementById("list");
+  list.innerHTML +=
+    "<li id=" +
+    id +
+    ">" +
+    text +
+    " <button onclick=\"deleteItem('" +
+    id +
+    "')\">X</button></li>";
+}
+
+function deleteItem(id) {
+  if (confirm("u want delete that?")) {
+    document.getElementById(id).style.display = "none";
+    unsetCookie(id);
+  }
+}
---
+++ cell03/ex04/ex00bis/background.js
@@ -0,0 +1,7 @@
+$("#btnSubmit").click(function () {
+  changeBackground();
+});
+function changeBackground() {
+  document.body.style.backgroundColor =
+    "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
